0c8a5a95dc4fb9cd789fa943a682a1290de133ce
William Richman <WRichma@mesaaz.gov>
2019-12-24 08:35:27 -0700

    added new js with jquery

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function generate() {
 
-
+    /* alert("Generator was called ") */
 
     const names = ["ALEGRIA A", "BAISA R", "CARRASCO F", "CHAIRA R", "DESSELLIER J", "FREEMAN L", "GOMEZ D", "HAINES T", "HERNANDEZ I", "MANSHAEM R", "MONTANO R", "PSHAK R", "ROHR D", "SMITH M", "AVAKIAN M", "CONNOLLY J", "ESPINO J", "GRAHAM T", "GREEN J", "HEARD B", "HOFMANN K", "KERR C", "MILLER K", "PERNACK G", "QUINTERO J ", "WADD C", "YEAGER D", "WALDER D", "BIETZ M", "CABLE D", "DAVIS Y", "GARDUNO D", "GUTIERREZ F", "HERNANDEZ M", "HORNBERGER Z", "LAVELLE B", "ORR J", "PROFFITT J ", "SAAVEDRA F", "SMITH E", "STAHL N", "TUMLINSON S", "ALICEA B", "BAKER J", "BERTRAND T", "BUGARIN A", "BURLESON R", "HESTER D", "JOHNSON R", "KENT P", "LAWRENCE B", "MORENO A", "MORENO JA", "MORENO JU", "SCHULTE D", "WOODS D", "ALVARADO M", "BLANCO R", "BROWN D", "HANSON S", "JONES J ", "LOPEZ R", "MCKINNEY R", "PADILLA D", "PERKINS J", "ROMERO R", "SALCIDO Z", "SIECKMANN S", "TISCHER T", "ZACHMAN D", "ADAMS R", "ANDERSON R", "CAMPBELL C", "CONOVER C", "FAIRBANKS W", "GLADEM R", "HIRSHBERG D", "KRAUSE R", "MAIN D", "MONTIJO F", "OCHOA R", "SALENTINE D", "STAMEY R", "TORRES S", "VIAR G"]
 
     /* console.log(names) */
-    let name = ""
+    let name = []
 
     for (let i = 0; i < names.length; i++) {
         /* let name = names[i] */
-        name = names[Math.floor(Math.random) * Math.floor(names.length - 1)];
+        const container = Math.floor(Math.random() * (names.length - 1));
+        name.push(names[container]);
+        console.log(container)
+        document.getElementById('target').innerHTML = container;
+
+        /*  names.splice(container, 1)
+         console.log(container) */
+
+
+
 
     }
+    return name
 
 };
-generate();
-console.log(generate)
+
+
+
+
+
+
+
+
 
 
 
